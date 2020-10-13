da0025bedf3f66df1b003aadeaf67ca96c8ce97d
charankavi <72601768+charankavi@users.noreply.github.com>
2020-10-13 11:37:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,6 +13,8 @@ var obstacleGroup;
 var score;
 var ground;
 var score = 0;
+var jungle;
+var gameover;
 
 function preload(){
   
@@ -22,6 +24,9 @@ function preload(){
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
 
+  jungle_image = loadImage("jungle.jpg");
+  
+  gameover_image = loadImage("gameOver.png");
 }
 
 function setup() {
@@ -31,11 +36,12 @@ createCanvas(400,400);
  monkey = createSprite(80,340,20,20);
  monkey.addAnimation("running", monkey_running);  
  monkey.scale=0.1;
- monkey.setCollider("rectangle",0,0,monkey.width,monkey.hight);  
+ monkey.setCollider("circle",0,0,monkey.width / 3);  
+
   
 //creating ground
-ground=createSprite(200,400,800,20);
-  
+ground=createSprite(200,350,800,20);
+ground.visible = false;  
 //creating bananaGroup
 bananaGroup = createGroup();  
   
@@ -44,24 +50,38 @@ obstacleGroup = createGroup();
   
 //setting gameState to play  
   gameState = PLAY;
+//creating jungle
+  jungle = createSprite(200,200,400,400);
+  jungle.addImage(jungle_image);
+  jungle.scale = 0.55;
+  jungle.depth = -50;
+  jungle.velocityX = -1;
+  
+  gameover = createSprite (200,200,100,100);
+  gameover.addImage(gameover_image);
+  gameover.visible = false;
+
+
 }
 
 function draw() {
   
 background("white");
 //adding ground 
-text("survival Time:" + score,100,50);   
+  
 //adding gravity 
 monkey.velocityY=monkey.velocityY+0.5;
-//making monkry collid the ground  
+//making monkey collid the ground  
 monkey.collide(ground); 
 
+  
 //destroying banana when monkey is touching them
 if (bananaGroup.collide(monkey)) {
       bananaGroup.destroyEach();
+     monkey.scale = monkey.scale + 0.025; 
     }
 //making monkey jump  
-if(keyDown("space") && monkey.y >= 350){
+if(keyDown("space") && monkey.y >= 300){
       monkey.velocityY= -15;
   } 
   
@@ -70,11 +90,23 @@ if(keyDown("space") && monkey.y >= 350){
   }
 
 if(obstacleGroup.isTouching(monkey)){  
-end();
-gameState = END;
+monkey.scale = monkey.scale - 0.05;
+obstacleGroup.destroyEach();
 }
- 
+  
+if(monkey.scale <= 0){
+end();
+gameState = END;  
+monkey.scale = 0;  
+}
+
+ if(jungle.x < 150){
+ jungle.x = 250;
+ }
  drawSprites();
+textSize(25); 
+fill("black");
+text("survival Time:" + score,10,50); 
 } 
  
 function spawnbanana(){
@@ -91,7 +123,7 @@ function spawnbanana(){
 function spawnObstacle(){
 if(frameCount % 150   === 0)
 {
-obstacle=createSprite(400,370,20,10);
+obstacle=createSprite(400,330,20,10);
 obstacle.setCollider("circle",0,0,150);  
 obstacle.addImage(obstacleImage);
 obstacle.lifetime=300;  
@@ -111,14 +143,13 @@ score = Math.ceil(frameCount/frameRate());
 function end (){
  ground.velocityX=0; 
 obstacle.velocityX=0;
-textSize(25);  
-fill(0);
-text("Game Over", 150, 180);
  obstacleGroup.setVelocityXEach(0);
 bananaGroup.setVelocityXEach(0);
 obstacleGroup.setLifetimeEach(-1);
 bananaGroup.setLifetimeEach(-1);
  monkey.velocityY = 0;
+  gameover.visible = true;
+  jungle.velocityX = 0;
 }
 
 
